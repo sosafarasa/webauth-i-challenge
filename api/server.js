35dffb264ccc6666049aff696786dfffd916080e
Agustina Sosa <agustinasf85@gmail.com>
2019-07-22 11:20:51 -0400

    Server running and files setup

--- api/server.js
+++ api/server.js
@@ -1,3 +1,29 @@
 const express = require('express');
 const helmet = require('helmet');
-const
+const dotenv = require('dotenv');
+
+//
+
+const authRouter = require('../auth/auth-router.js');
+const usersRouter = require('../users/users-router.js');
+
+//
+
+dotenv.config();
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter);
+
+//
+
+server.get('/', (req, res) => {
+    res.send(`<h1>Hey there, it's working!</h1>`);
+});
+
+//
+
+module.exports = server;
